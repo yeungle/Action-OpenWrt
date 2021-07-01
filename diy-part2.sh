3774f206bb5ebf0c79028df680a1dfe28ad33009
yeungle <75871729+yeungle@users.noreply.github.com>
2021-07-01 20:58:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,6 +3,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.1.1/g' package/base-files/files/bin/config_generate
-
-# Delete default Password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
