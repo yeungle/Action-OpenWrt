cc28aed7ca6c06f6fe63008a5ba7f4d26cbd1463
yeungle <75871729+yeungle@users.noreply.github.com>
2021-07-12 11:38:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,17 @@ rm -rf package/lean/luci-theme-argon
 
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/smartdns
 
+
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede package/luci-app-smartdns
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 git clone https://github.com/yeungle/luci-app-autotimeset.git package/luci-app-autotimeset
 git clone https://github.com/yeungle/luci-app-onliner.git package/luci-app-onliner
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash.git -b master package/luci-app-openclash
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon
 git clone https://github.com/fatelpc/luci-theme-edge.git package/luci-theme-edge
 
