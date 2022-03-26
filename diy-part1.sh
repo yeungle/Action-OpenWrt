e7e6bb48362eb8ab9d58b3fde82d0847f60e2db4
yeungle <75871729+yeungle@users.noreply.github.com>
2022-03-26 21:44:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,6 +29,7 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 git clone https://github.com/yeungle/luci-app-onliner.git package/luci-app-onliner
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon
 git clone https://github.com/fatelpc/luci-theme-edge.git package/luci-theme-edge
 
