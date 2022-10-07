ddd232fadf1f12f9c0fe6edb8c09f88e8cdd881d
yeungle <75871729+yeungle@users.noreply.github.com>
2022-10-07 16:06:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,18 +19,18 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 rm -rf feeds/packages/net/smartdns
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/smartdns
 
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede package/luci-app-smartdns
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 git clone https://github.com/yeungle/luci-app-onliner.git package/luci-app-onliner
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon
 git clone https://github.com/fatelpc/luci-theme-edge.git package/luci-theme-edge
 
 ./scripts/feeds update -a
