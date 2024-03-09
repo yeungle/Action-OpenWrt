1ee6f98330af3c9b04ef0b6ffd351be2622d5d69
yeungle <75871729+yeungle@users.noreply.github.com>
2024-03-09 20:09:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # rm -rf feeds/packages/net/smartdns
@@ -27,7 +27,7 @@
 # git clone https://github.com/pymumu/luci-app-smartdns.git -b lede package/luci-app-smartdns
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/fw876/helloworld/luci-app-ssr-plus.git package/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld/luci-app-ssr-plus.git package/luci-app-ssr-plus
 # git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 # git clone https://github.com/yeungle/luci-app-onliner.git package/luci-app-onliner
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
